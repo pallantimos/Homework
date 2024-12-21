45bf15ba169dbb7a9ae526d641e18fac0bd26579
Aldar <dondokovab@mer.ci.nsu.ru>
2024-12-21 14:15:49 +0700

    Исправление ошибок в названиях функций и переменных, добавлены docstring и анотации типов

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,16 @@
 def filter_by_state(list_dict: list, state='EXECUTED') -> list:
+    """ "Функция фильтрует список словарей по ключ state"""
     return [item for item in list_dict if item.get("state") == state]
 
 
-def choose_filter(sort_by: str):
-    if sort_by == 'убывание':
+def select_filter(sort_by: str) -> bool:
+    """ "Функция возвращает булевый тип в зависимости от сортировки"""
+    if sort_by == 'decreasing':
         return True
     else:
         return False
 
 
-def sort_by_date(list_dict: list, sort_by='убывание') -> list:
-    return sorted(list_dict, key=lambda x: x['date'], reverse=choose_filter(sort_by))
+def sort_by_date(list_dict: list, sort_by='decreasing') -> list:
+    """ "Функция сортирует список по дате"""
+    return sorted(list_dict, key=lambda x: x['date'], reverse=select_filter(sort_by))
