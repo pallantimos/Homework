337dad239a6e05f2b68d036549b38b7a0399f455
Aldar <dondokovab@mer.ci.nsu.ru>
2024-12-21 14:25:35 +0700

    В файл README.md добавлено 'Использование' и 'Пример использования'

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,7 @@ def filter_by_state(list_dict: list, state='EXECUTED') -> list:
     return [item for item in list_dict if item.get("state") == state]
 
 
-def select_filter(sort_by: str) -> bool:
+def choose_filter(sort_by: str) -> bool:
     """ "Функция возвращает булевый тип в зависимости от сортировки"""
     if sort_by == 'decreasing':
         return True
@@ -13,4 +13,4 @@ def select_filter(sort_by: str) -> bool:
 
 def sort_by_date(list_dict: list, sort_by='decreasing') -> list:
     """ "Функция сортирует список по дате"""
-    return sorted(list_dict, key=lambda x: x['date'], reverse=select_filter(sort_by))
+    return sorted(list_dict, key=lambda x: x['date'], reverse=choose_filter(sort_by))
