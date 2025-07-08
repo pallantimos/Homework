3c9b0a5be77efbf63bcc7900b10058085dae294f
Алдар Дондоков <dondokovabnsu@gmail.com>
2025-07-08 20:35:17 +0700

    Реализовано логирование в модулях utils и masks

--- src/processing.py
+++ src/processing.py
@@ -13,5 +13,6 @@ def choose_filter(sort_by: str) -> bool:
 
 def sort_by_date(list_dict: list, sort_by: str = "decreasing") -> list:
     """ "Функция сортирует список по дате"""
-    return sorted(list_dict, key=lambda x: x["date"],
-                  reverse=choose_filter(sort_by))
+    return sorted(
+        list_dict, key=lambda x: x["date"], reverse=choose_filter(sort_by)
+    )
